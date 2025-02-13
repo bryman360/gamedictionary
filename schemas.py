6df5beee9f8825be722c05f7422215db9f3366cc
bryman360 <bryman360@yahoo.com>
2025-02-12 17:02:15 -0800

    Schema restructure to include relationships

--- schemas.py
+++ schemas.py
@@ -1,25 +1,57 @@
 from marshmallow import Schema, fields
 
-class WordSchema(Schema):
+
+# ------------------------------------------------------------
+# 'Plain' Schemas (aka Schemas without relationships)
+# ------------------------------------------------------------
+
+class PlainWordSchema(Schema):
     word_id = fields.Str(dump_only=True)
     word = fields.Str(required=True)
     definition = fields.Str(required=True)
     example = fields.Str(required=True)
-    author_id = fields.Str(required=True)
     published = fields.Bool(dump_only=True)
     submit_datetime = fields.DateTime(dump_only=True)
 
+class PlainGameSchema(Schema):
+    game_id = fields.Str(dump_only=True)
+    game_name = fields.Str(required=True)
+    image_url = fields.Str()
+    wiki_url = fields.Str()
+
+class PlainUserSchema(Schema):
+    user_id = fields.Int(dump_only=True)
+    username = fields.Str(unique=True, required=True)
+
+
+# ------------------------------------------------------------
+# Base Schemas (aka Plain Schemas now including relationships)
+# ------------------------------------------------------------
+
+class WordSchema(PlainWordSchema):
+    author_id = fields.Int(required=True, load_only=True)
+    user = fields.Nested(PlainUserSchema(), dump_only=True)
+
+class GameSchema(Schema):
+    pass
+
+class UserSchema(PlainUserSchema):
+    words = fields.List(fields.Nested(PlainWordSchema()), dump_only=True)
+
+
+# ------------------------------------------------------------
+# Update Schemas
+# ------------------------------------------------------------
 
 class WordUpdateSchema(Schema):
     word = fields.Str()
     definition = fields.Str()
     example = fields.Str()
 
-
-class GameSchema(Schema):
-    game_id = fields.Str(dump_only=True)
-    game_name = fields.Str(required=True)
-
-
 class GameUpdateSchema(Schema):
     game_name = fields.Str()
+    image_url = fields.Str()
+    wiki_url = fields.Str()
+
+class UserUpdateSchema(Schema):
+    username = fields.Str()
