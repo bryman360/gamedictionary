5594c4464e4ce0c1cfee1477a050239e8ef176b2
bryman360 <bryman360@yahoo.com>
2025-02-24 12:33:26 -0800

    TODO comment for properly linking author/user to words

--- models/word.py
+++ models/word.py
@@ -8,6 +8,7 @@ class WordModel(db.Model):
     word = db.Column(db.String(50), unique=False, nullable=False)
     definition = db.Column(db.Text(), unique=False, nullable=False)
     example = db.Column(db.Text(), unique=False, nullable=False)
+    # TODO: Properly link to user_id
     author_id = db.Column(db.Integer, nullable=False)#, db.ForeignKey("users.user_id"), unique=False, nullable=False)
     published = db.Column(db.Boolean, unique=False, nullable=False)
     submit_datetime = db.Column(db.DateTime, unique=False, nullable=False)
