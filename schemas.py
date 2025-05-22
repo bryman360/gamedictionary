791f5cbc3e715cb3a2e725759976b366f2636134
bryman360 <bryman360@yahoo.com>
2025-05-22 11:50:22 -0700

    Is_active flags and search schemas

--- schemas.py
+++ schemas.py
@@ -12,17 +12,20 @@ class PlainWordSchema(Schema):
     example = fields.Str(required=True)
     published = fields.Bool(dump_only=True)
     submit_datetime = fields.DateTime(dump_only=True)
+    is_active = fields.Int(dump_only=True)
 
 class PlainGameSchema(Schema):
     game_id = fields.Str(dump_only=True)
     game_name = fields.Str(required=True)
     image_url = fields.Str()
     wiki_url = fields.Str()
+    is_active = fields.Int(dump_only=True)
 
 class PlainUserSchema(Schema):
     user_id = fields.Int(dump_only=True)
     username = fields.Str(unique=True, required=True)
     password = fields.Str(required=True, load_only=True)
+    is_active = fields.Int(dump_only=True)
 
 
 # ------------------------------------------------------------
@@ -63,3 +66,35 @@ class GameUpdateSchema(Schema):
 class UserUpdateSchema(Schema):
     username = fields.Str()
     password = fields.Str()
+
+
+# ------------------------------------------------------------
+# Minimum Info Schemas
+# ------------------------------------------------------------
+
+class MinimumWordSchema(Schema):
+    word_id = fields.Str()
+    word = fields.Str()
+
+
+# ------------------------------------------------------------
+# Search Schemas
+# ------------------------------------------------------------
+
+class WordsSearchSchema(Schema):
+    word = fields.Str()
+    page = fields.Int()
+    startsWith = fields.Str()
+
+class GamesSearchSchema(Schema):
+    name = fields.Str()
+    page = fields.Int()
+    startsWith = fields.Str()
+
+
+# ------------------------------------------------------------
+# Search Result Schemas
+# ------------------------------------------------------------
+
+class GameSearchResultSchema(PlainGameSchema):
+    words = fields.Nested(MinimumWordSchema)
