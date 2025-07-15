04f54e0dbe380182d812c8486405ca5946fd715f
bryman360 <bryman360@yahoo.com>
2025-07-14 23:39:43 -0700

    Flag handles an 'other' option and formats well before being sent

--- resources/flag.py
+++ resources/flag.py
@@ -1,12 +1,10 @@
 import os
 
-from flask import current_app
+from flask import current_app, jsonify
 from flask.views import MethodView
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_mail import Message, Mail
-from flask_smorest import Blueprint
-
-import json
+from flask_smorest import Blueprint, abort
 
 from models import WordModel, GameModel, UserModel
 from schemas import FlagSchema
@@ -22,35 +20,48 @@ class Flag(MethodView):
     def post(self, request_payload: dict):
         # Link, word, game, other
         content_type = request_payload['content_type']
-        content_id = request_payload['id']
-
-        if content_type == 'word':
-            content = WordModel.query.first_or_404(content_id)
-        elif content_type == 'game':
-            content = GameModel.query.first_or_404(content_id)
-
-        user = UserModel.query.first_or_404(1)#get_jwt_identity())
         reason = request_payload['reason']
+        user = UserModel.query.first_or_404(1)#get_jwt_identity())
+        non_content_specific_line = f'\n\nReason: "{reason}"\n\nby User: {{Username: {user.username}, User ID: {user.user_id}}}'
 
-        subject = 'Flag for ' + content_type.upper() + ' ' + str(content_id)
-        pretty_content = '{\n'
-        for key, value in content.as_dict().items():
-            pretty_content += f'\t\'{key}\':'
-            if type(value) == str:
-                pretty_content += f' \'{value}\',\n'
-            else:
-                pretty_content += f' {value},\n'
+        if content_type == 'other':
+            if 'description' not in request_payload:
+                abort(405)
+            
+            subject = f'Flag for Uncategorized Reason'
 
-        pretty_content += '}'
-        print(pretty_content)
+            body = f'Flagging for "Other" reason.\n\nDescription: {request_payload['description']}\n\n'
+            body += non_content_specific_line
+            
+        else:
+            if 'id' not in request_payload:
+                abort(405)
+            content_id = request_payload['id']
 
-        msg = Message(subject, sender=os.getenv('SEND_EMAIL'), recipients=[os.getenv('FLAG_RECV_EMAIL')])
-        msg.body = f'Flagging for {content_type} with ID {content_id}.\n'
-        msg.body += pretty_content
+            if content_type == 'word':
+                content = WordModel.query.first_or_404(content_id)
+            elif content_type == 'game':
+                content = GameModel.query.first_or_404(content_id)
 
-        msg.body += '\n\nReason: "' + reason + '"\nby User: { Username: ' + user.username + ', User ID: ' + str(user.user_id) + '}'
+
+            subject = 'Flag for ' + content_type.upper() + ' ' + str(content_id)
+            pretty_content = '{\n'
+            for key, value in content.as_dict().items():
+                pretty_content += f'\t\'{key}\':'
+                if type(value) == str:
+                    pretty_content += f' \'{value}\',\n'
+                else:
+                    pretty_content += f' {value},\n'
+
+            pretty_content += '}'
+
+            body = f'Flagging for {content_type} with ID {content_id}.\n\n'
+            body += pretty_content
+            body += non_content_specific_line
 
         with current_app.app_context():
+            msg = Message(subject, sender=os.getenv('SEND_EMAIL'), recipients=[os.getenv('FLAG_RECV_EMAIL')])
+            msg.body = body
             mail = Mail()
             mail.send(msg)
-        return {}
+        return jsonify({'message': 'Successfully flagged content.'})
