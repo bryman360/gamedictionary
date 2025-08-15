ca466ca4c4edae85e1912c18789a64d4ff898f4e
bryman360 <bryman360@yahoo.com>
2025-08-15 10:40:31 -0700

    Switch to using a straight query count instead of reading a file

--- resources/utils.py
+++ resources/utils.py
@@ -2,22 +2,18 @@ from flask import abort
 from flask.views import MethodView
 from flask_smorest import Blueprint
 from json import load as jsonload
+from models import WordModel, GameModel, UserModel
 
 blp = Blueprint('Utils', __name__, 'Blueprint for Utility functions.')
 
 @blp.route('/stats')
 class Stats(MethodView):
     def get(self):
-        game_count = None
-        word_count = None
-        with open('metadata.json', 'r') as metadata:
-            loaded_data = jsonload(metadata)
-            game_count = loaded_data['game_count']
-            word_count = loaded_data['word_count']
-        if not game_count or not word_count:
-            abort(400)
+        word_count = WordModel.query.count()
+        game_count = GameModel.query.count()
+        user_count = UserModel.query.count()
         
-        return {'game_count': game_count, 'word_count': word_count}, 200
+        return {'game_count': game_count, 'word_count': word_count, 'user_count': user_count}, 200
 
 @blp.route('/')
 class Health(MethodView):
